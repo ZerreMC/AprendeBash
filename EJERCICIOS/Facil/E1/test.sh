2799c41ae50811dd1e6b4b6cf9f2b46637e81339
Scirga <69958471+ZerreMC@users.noreply.github.com>
2024-02-29 21:24:09 +0100

    Update test.sh

--- EJERCICIOS/Facil/E1/test.sh
+++ EJERCICIOS/Facil/E1/test.sh
@@ -10,14 +10,21 @@
 testing(){
     local valorInicial=$1
     local valorEsperado=$2
+    local tipoPrueba=$3
 
-    local resultado=$(echo "$valorInicial" | ./absoluto.sh | grep -o '[0-9]*')
+    local resultado
+
+    if [ "$tipoPrueba" == "num" ]; then
+        resultado=$(echo "$valorInicial" | ./absoluto.sh | grep -o '[0-9]*')
+    else
+        resultado=$(echo "$valorInicial" | ./absoluto.sh 2>&1 >/dev/null)
+    fi
 
     printf "\nPrueba con valor %s: " "$valorInicial"
 
-    if [[ "$resultado" -eq "$valorEsperado" ]]; then
+    if [[ "$resultado" == "$valorEsperado" ]]; then
         echo -e "\e[1;32m Prueba superada\e[0m"
-    else 
+    else
         echo -e "\e[1;31m Prueba no superada\e[0m"
         exit 1
     fi
@@ -27,15 +34,18 @@ testing(){
 main(){
     
     # Test con números positivos
-    testing 1 1
-    testing 1000000 1000000
+    testing 1 1 num
+    testing 1000000 1000000 num
 
     # Test con números negativos
-    testing -1 1
-    testing -1000000 1000000
+    testing -1 1 num
+    testing -1000000 1000000 num
     
     # Test con el 0
-    testing 0 0
+    testing 0 0 num
+
+    # Test en el canal de errores
+    testing "asdfgh" "ERROR - No es un valor válido" err
 
     echo -e "\e[42mEjercicio aprobado\e[0m"
 }
